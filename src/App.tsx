6a9d27a67b0b40dca42d7865c3f53981da17ee1a
XeNeeX <kamiljarmolowicz9@gmail.com>
2023-10-11 20:58:18 +0200

    Added first Landing Page Shortcut Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import {Navbar} from './components/Navbar';
 import {LandingPagePicture} from './components/LandingPagePicture'
+import {LandingPageShortcutButtons} from "./components/LandingPageShortcutButtons";
 import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
       <>
           <Navbar/>
           <LandingPagePicture/>
+          <LandingPageShortcutButtons/>
       </>
   )
 }
---
+++ src/components/LandingPageShortcutButtons.tsx
@@ -0,0 +1,15 @@
+import './component_css/landing_sc_buttons.scss'
+export function LandingPageShortcutButtons(){
+    return(
+        <>
+            <article className={"scParent"}>
+                <section className={"scMap"}>
+                    <div className={"cross"}></div>
+                    <span className={"scMapText"}>Nasze<br/>placówki</span>
+                </section>
+                Znajdź naszą placówkę najbliżej siebie!
+            </article>
+
+        </>
+    )
+}
